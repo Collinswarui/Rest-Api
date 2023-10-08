e0bdaefc8cbe54caae541afdc6d333501f195669
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-10-08 22:28:54 +0300

    added the logic for all the endpoints in my backend

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,22 +1,19 @@
 // set up the routes for the apis
 const express = require('express');
 const router = express.Router();
-const { getGoals } = require('../controllers/getController');
+const { getGoals, 
+        setGoal, 
+        updateGoal, 
+        deleteGoal } = require('../controllers/getController');
 
 // create an api endpoint to get all the goals
 router.get('/', getGoals);
 
-router.post('/', (req, res) => {
-    res.status(200).json({ message: 'Set goal'})
-});
+router.post('/', setGoal);
 
-router.put('/:id', (req, res) => {
-    res.status(200).json({ message: `update goal ${req.params.id}`})
-});
+router.put('/:id', updateGoal);
 
-router.get('/:id', (req, res) => {
-    res.status(200).json({ message: `update goal ${req.params.id}`})
-});
+router.get('/:id', deleteGoal);
 
 
 module.exports = router;
