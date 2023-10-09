a73179ebb7d97039a9b182976af66b0d7d961077
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-10-09 16:15:08 +0300

    Added an async hanler to handle errors  in the controller

--- backend/controllers/getController.js
+++ backend/controllers/getController.js
@@ -1,36 +1,38 @@
 // controller to handle the logic
 
+const asyncHandler = require('express-async-handler')
+
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
-const getGoals =  (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get goals'})
-}
+})
 
 // @desc Set goal
 // @route POST /api/goals
 // @access Private
-const setGoal =  (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
     if (!req.body.text) {
         res.status(400)
         throw new Error('please add a text field')
     }
     
-}
+})
 
 // @desc Update goal
 // @route PUT /api/goals/:id
 // @access Private
-const updateGoal =  (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `update goal ${req.params.id}`})
-}
+})
 
 // @desc Delete goal
 // @route DELETE /api/goals/:id
 // @access Private
-const deleteGoal =  (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `update goal ${req.params.id}`})
-}
+})
 
 
 
--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,6 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`)); 
