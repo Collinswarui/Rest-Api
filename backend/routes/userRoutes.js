e0edc0f6329e04c73a9ac05001556f6365c41b68
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-10-12 22:59:20 +0300

    created a user controller to handle the logic to create the user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require ('express')
-const route = express.Router()
+const router = express.Router()
 const { registerUser } = require('../controllers/userController')
 
 router.post('/', registerUser)
