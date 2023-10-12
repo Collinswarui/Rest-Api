e639cfaa4e9308533c7469f27bcea51579a37fe6
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-10-12 23:36:02 +0300

    modified user controller to handle the logic for login and get user data

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,26 @@
+// @desc Register new user
+// @route POST /api/users 
+// @access Public
 const registerUser = (req, res)  => {
     res.json({message: 'Register User'})
 }
 
+// @desc Authenticate user
+// @route POST /api/users/login
+// @access Public
+const loginUser = (req, res)  => {
+    res.json({message: 'Login User'})
+}
+
+// @desc Get user 
+// @route GET /api/users/me
+// @access Public
+const getMe = (req, res) => {
+    res.json({ message: 'User data'})
+}
+
 module.exports = {
     registerUser,
+    loginUser,
+    getMe,
 }
