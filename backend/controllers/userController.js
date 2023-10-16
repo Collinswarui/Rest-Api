dd45e685a815336db9d670ec43bbd099dbe5283f
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-10-16 22:56:59 +0300

    added asyc handler to the user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,21 +1,26 @@
+const jwt = require('jsonwebtoken')
+const  bcrypt = require('bcryptjs')
+const asyncHandler =  require('express-async-handler')
+const User = require('../models/userModel')
+
 // @desc Register new user
 // @route POST /api/users 
 // @access Public
-const registerUser = (req, res)  => {
+const registerUser = async(req, res)  => {
     res.json({message: 'Register User'})
 }
 
 // @desc Authenticate user
 // @route POST /api/users/login
 // @access Public
-const loginUser = (req, res)  => {
+const loginUser = async(req, res)  => {
     res.json({message: 'Login User'})
 }
 
 // @desc Get user 
 // @route GET /api/users/me
 // @access Public
-const getMe = (req, res) => {
+const getMe = async(req, res) => {
     res.json({ message: 'User data'})
 }
 
