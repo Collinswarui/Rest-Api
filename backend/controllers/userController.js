71f57739665f1e524cbeb8e1337f59f737e88114
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-10-16 23:33:20 +0300

    added logic to create user and Hash the password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,7 +11,7 @@ const registerUser = asyncHandler(async(req, res)  => {
 
     if(!name || !email || !password) {
         res.status(400)
-        throw new Error('please add all fields')
+        throw new Error('Please add all fields')
     }
 
     // check if user exists
@@ -22,7 +22,28 @@ const registerUser = asyncHandler(async(req, res)  => {
         throw new Error('User already exists')
     }
 
-    res.json({message: 'Register User'})
+    // Hash the password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // create suer
+    const user = await User.create({
+        name,
+        email,
+        password:hashedPassword
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        }) 
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+
 })
 
 // @desc Authenticate user
