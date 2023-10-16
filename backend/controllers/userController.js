155da8083e48328095a065e97cc08fc9854cf0cb
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-10-16 23:14:59 +0300

    New user registration and check if a user exists

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,23 +6,38 @@ const User = require('../models/userModel')
 // @desc Register new user
 // @route POST /api/users 
 // @access Public
-const registerUser = async(req, res)  => {
+const registerUser = asyncHandler(async(req, res)  => {
+    const { name, email, password } = req.body
+
+    if(!name || !email || !password) {
+        res.status(400)
+        throw new Error('please add all fields')
+    }
+
+    // check if user exists
+    const userExists = await User.findOne({email})
+
+    if(userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
     res.json({message: 'Register User'})
-}
+})
 
 // @desc Authenticate user
 // @route POST /api/users/login
 // @access Public
-const loginUser = async(req, res)  => {
+const loginUser = asyncHandler(async(req, res)  => {
     res.json({message: 'Login User'})
-}
+})
 
 // @desc Get user 
 // @route GET /api/users/me
 // @access Public
-const getMe = async(req, res) => {
+const getMe = asyncHandler(async(req, res) => {
     res.json({ message: 'User data'})
-}
+})
 
 module.exports = {
     registerUser,
