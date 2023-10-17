4cc1402c33924e60d22f2b9830516eef6436691e
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-10-17 13:48:01 +0300

    Protecting goal routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -4,13 +4,14 @@ const router = express.Router();
 const { getGoals, 
         setGoal, 
         updateGoal, 
-        deleteGoal } = require('../controllers/getController');
+        deleteGoal } = require('../controllers/goalController')
+const { protect } = require('../middleware/authMiddleware')
 
 // create an api endpoint to get all the goals
 
 // endpoint for both the GET and POST methods
-router.route('/').get(getGoals).post(setGoal);
-router.route('/:id').delete(deleteGoal).put(updateGoal);
+router.route('/').get(protect, getGoals).post(protect, setGoal);
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal);
 
 
 module.exports = router;
