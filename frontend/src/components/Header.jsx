19689e0d23c833b39b32199015b8cc72fa686c84
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-10-18 23:29:12 +0300

    added a header to my application

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <header className='header'>
+        <div className="logo">
+            <ul>
+                <li>
+                    <Link to='/login'>
+                        <FaSignInAlt /> Login
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/register'>
+                        <FaUser /> Register
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    </header>
+  )
+}
+
+export default Header
