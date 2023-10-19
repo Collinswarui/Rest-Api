6e31d6920b546976a91060407692c8acb0c1b360
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-10-19 23:20:51 +0300

    added reducers to the auth

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -12,5 +12,18 @@ const initialState = {
 }
 
 export const authSlice = createSlice({
-    
+    name: 'auth',
+    initialState,
+    reducers: {
+        reset: (state) => {
+            state.isLoading = false
+            state.isSuccess = false
+            state.isError = false
+            state.message = ''
+        }
+    },
+    extraReducers: () => {}
 })
+
+export const {reset} = authSlice.actions
+export default authSlice.reducer
