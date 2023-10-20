e45a0df75d4c6e3fdccace267e886047133b2544
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-10-21 01:54:34 +0300

    added logic to login user

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -14,6 +14,19 @@ const register = async (userData) => {
 
 }
 
+
+// Login user
+const login = async (userData) => {
+    const response = await axios.post(API_URL + 'login', userData)
+
+    if(response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+
+    return response.data
+
+}
+
 // Logout User
 const logout = () => {
     localStorage.removeItem('user')
@@ -23,6 +36,7 @@ const logout = () => {
 const authService = {
     register,
     logout,
+    login,
 }
 
 export default authService
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,11 @@
 import { useState, useEffect } from "react"
 import {FaSignInAlt } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { login, reset } from "../features/auth/authSlice"
+import Spinner from '../components/Spinner'
+
 
 
 function Login() {
@@ -12,6 +18,25 @@ function Login() {
 
   const { email, password } = formData
 
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth)
+
+    useEffect(() => {
+      if(isError) {
+        toast.error(message)
+      }
+  
+      if(isSuccess || user) {
+        navigate('/')
+      }
+  
+      dispatch(reset())
+    }, [user, isError, isSuccess, message, navigate, dispatch])
+  
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -21,6 +46,17 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault()
+
+    const userData = {
+      email,
+      password,
+    }
+
+    dispatch(login(userData))
+  }
+
+  if(isLoading) {
+    return <Spinner />
   }
 
 
