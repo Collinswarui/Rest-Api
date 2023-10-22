71f85e331f15b330d96d3c10b836b94dca781c62
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-10-23 01:13:05 +0300

    imported goalService into the goal slice

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -1,4 +1,5 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import goalService from './goalService'
 
 
 const initialState = {
@@ -31,6 +32,22 @@ export const goalSlice = createSlice({
     initialState,
     reducers:{
         reset: () => initialState
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(createGoal.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(createGoal.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.goals.push(action.payload)
+            })
+            .addCase(createGoal.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
     }
 })
 
