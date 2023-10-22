82e28a1a39e6edfb142cf3339641f544be24231c
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-10-22 22:46:31 +0300

    modified getMe function in the user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -74,13 +74,13 @@ const loginUser = asyncHandler(async(req, res)  => {
 // @route GET /api/users/me
 // @access Private
 const getMe = asyncHandler(async(req, res) => {
-    const { _id, name, email } = await User.findById(req.user.id)
+    // const { _id, name, email } = await User.findById(req.user.id)
 
-    res.status(200).json({
-        id: _id,
-        name,
-        email,
-    })
+    res.status(200).json(req.user)
+        // id: _id,
+        // name,
+        // email,
+    
 })
 
 // Generate JWT
