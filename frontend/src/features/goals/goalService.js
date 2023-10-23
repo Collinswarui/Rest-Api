23b0e15bb051b61663310dfbde3c87e5688a4663
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-10-23 15:21:04 +0300

    added a getGoals funtion to get user goals in the UI

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const API_URL = 'api/goals'
 
-// reate new goal
+// create new goal
 const createGoal = async (goalData, token) => {
     const config = {
         headers: {
@@ -15,8 +15,24 @@ const createGoal = async (goalData, token) => {
     return response.data
 }
 
+
+
+// get user goals
+const getGoals = async (token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    const response = await axios.get(API_URL, config)  
+
+    return response.data
+}
+
 const goalService = {
-    createGoal
+    createGoal,
+    getGoals
 }
 
 export default goalService
