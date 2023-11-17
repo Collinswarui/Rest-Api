1541e98af17d407985fcf10f89f06403e1a00697
Collins Wanjiru <waruiwanjiru47@gmail.com>
2023-11-17 22:18:24 +0300

    debug server

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv').config();
 const colors = require('colors');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 connectDB();
 
